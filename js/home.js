85ad4e8783f6bad5cf7061d5f8efb71a92a4bd36
schollsebastian <scholl.sebastian@gmx.at>
2019-03-20 13:35:15 +0100

    added mosemove event

--- js/home.js
+++ js/home.js
@@ -1,11 +1,18 @@
-window.addEventListener('load', () => {
+let background;
+let originalCoordinates;
+
+window.addEventListener('load', init);
+
+function init(event) {
+    background = document.getElementById('backgroundImg');
     resize();
+
     window.addEventListener('resize', resize);
-});
+    window.addEventListener('mousemove', mousemove);
+    window.removeEventListener('load', init);
+}
 
 function resize() {
-    let background = document.getElementById('backgroundImg');
-
     if (background.width / background.height < innerWidth / innerHeight) {
         background.height = background.height / background.width * innerWidth;
         background.width = innerWidth;
@@ -19,4 +26,13 @@ function resize() {
 
     background.style.marginLeft = (background.width - innerWidth) / -2 + 'px';
     background.style.marginTop = (background.height - innerHeight) / -2 + 'px';
+}
+
+function mousemove(event) {
+    if (originalCoordinates !== undefined) {
+        background.style.marginLeft = parseFloat(background.style.marginLeft) + (event.clientX - originalCoordinates.x) / 80 + 'px';
+        background.style.marginLeft = parseFloat(background.style.marginTop) + (event.clientY - originalCoordinates.y) / 80 + 'px';
+    }
+    
+    originalCoordinates = {x: event.clientX, y: event.clientY};
 }
