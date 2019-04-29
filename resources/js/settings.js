40c415c5b8fdf433daca32781fbb2f2b7bf8e718
schollsebastian <scholl.sebastian@gmx.at>
2019-04-30 00:01:38 +0200

    removed settings page

--- resources/js/settings.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-window.addEventListener('load', init);
-
-function init() {
-    window.removeEventListener('load', init);
-
-    document.getElementById('load').style.opacity = 0;
-}
