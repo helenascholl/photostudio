5ae233cbc0baea768a7fc996f943b26165fcb60b
schollsebastian <scholl.sebastian@gmx.at>
2019-05-28 22:55:53 +0200

    filenames are read from database

--- assets/js/yourphotos.js
+++ assets/js/yourphotos.js
@@ -7,8 +7,49 @@ function init() {
 
     firebase.auth().onAuthStateChanged((user) => {
         if (!user) {
-            document.getElementById('popup').style.display = 'flex';
+            document.getElementById('noAccount').style.display = 'flex';
             document.getElementById('content').style.display = 'none';
+        } else {
+            firebase.database().ref(`images/${user.uid}`).once('value').then((snapshot) => {
+                let data = snapshot.val();
+
+                if (data) {
+                    for (let nameString of data) {
+                        let photo = document.createElement('div');
+                        let file = document.createElement('div');
+                        let filenameDiv = document.createElement('div');
+                        let fileImage = document.createElement('i');
+                        let splitNameString = nameString.split('-');
+                        let filename = splitNameString[0];
+
+                        photo.className = 'photo';
+                        file.className = 'file';
+                        filenameDiv.className = 'filename';
+                        fileImage.className = 'fa fa-file-image-o';
+
+                        filenameDiv.textContent = filename;
+                        filenameDiv.title = filename;
+
+                        photo.date = {
+                            year: splitNameString[1],
+                            month: splitNameString[2],
+                            day: splitNameString[3],
+                            hour: splitNameString[4],
+                            minute: splitNameString[5],
+                            second: splitNameString[6]
+                        };
+
+                        file.appendChild(fileImage);
+                        photo.appendChild(file);
+                        photo.appendChild(filenameDiv);
+                        document.getElementById('content').appendChild(photo);
+                    }
+                } else {
+                    // no photos
+                }
+            }).catch((error) => {
+                console.log(error.message);
+            });
         }
     });
 
