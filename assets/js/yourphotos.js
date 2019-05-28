aca1552b9100154cb7fab6fe2d7a5bde310cac81
schollsebastian <scholl.sebastian@gmx.at>
2019-05-29 00:34:00 +0200

    added context menu

--- assets/js/yourphotos.js
+++ assets/js/yourphotos.js
@@ -8,10 +8,11 @@ function init() {
     firebase.auth().onAuthStateChanged((user) => {
         if (!user) {
             document.getElementById('noAccount').style.display = 'flex';
-            document.getElementById('content').style.display = 'none';
+            document.getElementById('photos').style.display = 'none';
         } else {
             firebase.database().ref(`images/${user.uid}`).once('value').then((snapshot) => {
                 let data = snapshot.val();
+                let delay = 1;
 
                 if (data) {
                     for (let nameString of data) {
@@ -23,12 +24,16 @@ function init() {
                         let filename = splitNameString[0];
 
                         photo.className = 'photo';
+                        photo.style.opacity = 0;
+                        photo.style.transform = 'scale(0, 0)';
                         file.className = 'file';
                         filenameDiv.className = 'filename';
                         fileImage.className = 'fa fa-file-image-o';
 
                         filenameDiv.textContent = filename;
                         filenameDiv.title = filename;
+
+                        photo.filename = nameString;
 
                         photo.date = {
                             year: splitNameString[1],
@@ -39,10 +44,17 @@ function init() {
                             second: splitNameString[6]
                         };
 
+                        photo.addEventListener('contextmenu', openOptions);
+
                         file.appendChild(fileImage);
                         photo.appendChild(file);
                         photo.appendChild(filenameDiv);
-                        document.getElementById('content').appendChild(photo);
+                        document.getElementById('photos').appendChild(photo);
+
+                        setTimeout(() => {
+                            photo.style.opacity = 1;
+                            photo.style.transform = 'scale(1, 1)';
+                        }, delay += 10);
                     }
                 } else {
                     // no photos
@@ -82,4 +94,14 @@ function initFirebase() {
         storageBucket: "webprojekt-bf181.appspot.com",
         messagingSenderId: "403269192570"
     });
+}
+
+function openOptions(event) {
+    let options = document.getElementById('options');
+
+    event.preventDefault();
+
+    options.style.left = event.clientX + 'px';
+    options.style.top = event.clientY + 'px';
+    options.style.display = 'flex';
 }
